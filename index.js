620877eb6a817b90d1607908634115db835e3c13
damwead <mikhal3vsky@gmail.com>
2022-09-19 15:29:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// function palindrome(str=str) {
+//   str.strip()
+// }
+
+let str = "2_A3*3#A2";
+
+let clean_str = "";
+
+for(let i = 0; i < str.length; i++) {
+  if(str[i].)
+    console.log(str[i])
+}
+
+// palindrome("2_A3*3# A2");
