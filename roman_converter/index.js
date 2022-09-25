656ffe6f7c41cbd85256705ebde613c1d60bbaf8
damwead <mikhal3vsky@gmail.com>
2022-09-25 17:38:22 +0200

    added commets

--- roman_converter/index.js
+++ roman_converter/index.js
@@ -1,34 +1,36 @@
 // num should be <= 9999
 function convertToRoman(num) {
-  num = num.toString();
-  let roman = [];
+  num = num.toString(); // thus we can iterate through it
+  let inRoman = []; // result in roman
   let last = num.length- 1;
+  console.log(last)
 
   const roms = [["IX" ,"V","IV", "I"], ["XC","L", "XL","X"], ["CM","D", "CD","C"] ]
+  // при 29 прога идет с середины 1, и в конец поэтому выходит IIXC
 
   for(let i = last; i >= 0; i--) {
     let arr = roms[i]
 
     if(last == 4 && i == 0) {
-      roman.unshift("M".repeat(num[i]))
+      inRoman.unshift("M".repeat(num[i]))
     }
 
     if (num[i] == 4 || num[i] ==  9) {
       if(num[i] == 4) {
-        roman.push(arr[2])
+        inRoman.unshift(arr[2])
       } else {
-        roman.push(arr[0])
+        inRoman.unshift(arr[0])
       }
     } else if(num[i] == 5) {
-      roman.push(arr[1])
+      inRoman.unshift(arr[1])
     } else if (num[i] > 5) {
-      roman.push(arr[1]+arr[3].repeat(num[i]-5))
+      inRoman.unshift(arr[1]+arr[3].repeat(num[i]-5))
     } else {
-      roman.push(arr[3].repeat(num[i]))
+      inRoman.unshift(arr[3].repeat(num[i]))
     }
   }
 
- return roman.join("");
+ return inRoman.join("");
 }
 
 
@@ -41,24 +43,24 @@ console.log(convertToRoman(29));
 
 // if (num[last] == 4 || num[last] ==  9) {
 //   if(num[last] == 4) {
-//     roman.unshift("IV")
+//     inRoman.unshift("IV")
 //   } else {
-//     roman.unshift("IX")
+//     inRoman.unshift("IX")
 //   }
 // } else if (num[last] > 5) {
-//   roman.unshift(`V${"I".repeat(num[last]-5)}`)
+//   inRoman.unshift(`V${"I".repeat(num[last]-5)}`)
 // } else {
-//   roman.unshift(`${"I".repeat(num[last])}`)
+//   inRoman.unshift(`${"I".repeat(num[last])}`)
 // }
 
 // if (num[last-1] == 4 || num[last-1] ==  9) {
 //   if(num[last-1] == 4) {
-//     roman.unshift("XL")
+//     inRoman.unshift("XL")
 //   } else {
-//     roman.unshift("XC")
+//     inRoman.unshift("XC")
 //   }
 // } else if (num[last-1] > 5) {
-//   roman.unshift(`L${"X".repeat(num[last-1]-5)}`)
+//   inRoman.unshift(`L${"X".repeat(num[last-1]-5)}`)
 // } else {
-//   roman.unshift(`${"X".repeat(num[last-1])}`)
+//   inRoman.unshift(`${"X".repeat(num[last-1])}`)
 // } 
